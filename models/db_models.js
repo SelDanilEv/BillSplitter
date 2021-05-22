4cbd463bb3a350be186fa83d8d7e9ecfc2ab83e1
SelDanilEv <47692381+SelDanilEv@users.noreply.github.com>
2021-05-23 00:06:33 +0300

    fix

--- models/db_models.js
+++ models/db_models.js
@@ -1,3 +1,3 @@
 const sequelize = require("../db_connection.js");
-const models = {ROOM, USER_ROOM, USER, REQUEST} = require('..\\models\\db_schema').ORM(sequelize);
+const models = {ROOM, USER_ROOM, USER, REQUEST} = require('../models/db_schema').ORM(sequelize);
 module.exports = models;
